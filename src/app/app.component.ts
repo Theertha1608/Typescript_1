aa2e8454df2b98b53d39e99de1ed219444602177
theetha1608 <theerthakunjusss@gmail.com>
2024-03-14 18:43:36 +0530

    Finished the Typescript first task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,32 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Typescript';
+
+  ngOnInit(): void {
+    
+    this.example();
+  }
+  
+  example() {
+    var variable1 = 'var Variable';
+    let variable2 = 'let Variable';
+    const variable3 = 'const Variable';
+    let variable4 = 'Typescript';
+
+    if(true) {
+      var variable1 = 'var';
+      let variable2 = 'let';
+      let variable4 = 'name';
+      
+      console.log('1', variable1)
+      console.log('2', variable2)
+      console.log('3', variable3)
+      console.log('4', variable4)
+    }
+    
+    console.log('5', variable1)
+    console.log('6', variable2)
+    console.log('7', variable3)
+    console.log('8', variable4)
+  }
 }
